be0c99d04db5e41524e074c486fe501f0490ea30
Grauero <43115501+Grauero@users.noreply.github.com>
2018-10-01 17:28:18 +0300

    Changed source-maps config

--- react/webpack-configuration/webpack.dev.js
+++ react/webpack-configuration/webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: '../dist',
     hot: true,
