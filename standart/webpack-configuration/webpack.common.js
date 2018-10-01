3d5d78ad948686bf3c16b9626a9e71faf3eff387
Grauero <grayero6996@gmail.com>
2018-10-01 11:09:22 +0300

    Move webpack-configs to folder

--- standart/webpack-configuration/webpack.common.js
+++ standart/webpack-configuration/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname, '..', 'dist'),
   },
   module: {
     rules: [
@@ -60,8 +60,8 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
         title: 'title'
-      }),
+    }),
   ],
 };
