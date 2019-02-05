005f6432d7268164a669c1ba75cf64b969451f8b
Grauero <grayero6996@gmail.com>
2019-02-05 11:25:36 +0300

    Update test configs for mounting component

--- typescript + react/jest.config.js
+++ typescript + react/jest.config.js
@@ -1,14 +1,16 @@
 module.exports = {
+  preset: 'ts-jest',
   verbose: true,
   testEnvironment: 'jsdom',
-  setupTestFrameworkScriptFile: './__mocks__/jest.setup.js',
+  setupFilesAfterEnv: ['./__mocks__/jest.setup.ts'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg)$': './__mocks__/fileMock.js'
   },
   transform: {
+    '^.+\\.(ts|js)x?$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest'
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)x?$',
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
